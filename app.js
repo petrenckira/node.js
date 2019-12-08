5cc80164feb29e3a8bf7e71182d66a58dad6152c
Iryna Petrenko <iryna_petrenko1@epam.com>
2019-12-08 17:25:54 +0200

    Added logic for converting csv to json

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-//write code here
+const csv = require('csvtojson');
+const fs = require('fs');
+const csvFilePath = './csv/node_mentoring_t1_2_input_example.csv';
+const txtFilePath = 'csvToJson.txt';
+
+const readStream = fs.createReadStream(csvFilePath);
+const writeStream = fs.createWriteStream(txtFilePath);
+const csvEntity = csv().on('data',(data) => {
+                            const jsonStr = data.toString('utf8');
+                            writeStream.write(jsonStr);
+                          });
+
+readStream.pipe(csvEntity)
+          .on('error', function(e) { handleError(e)});
+
+function handleError(error) {
+  console.error(error);
+}
